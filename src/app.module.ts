f1742ecd7eb59e5a63b6bf86fb0d36799f0aaa3b
queirozmarcondes <renatodequeirozmarcondes@gmail.com>
2025-05-13 20:55:48 -0300

    criando recursos tasks

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,14 @@ import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './users/infra/database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { LoggerService } from './log/logger.service';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    AuthModule, UsersModule, DatabaseModule,
+    AuthModule, UsersModule, DatabaseModule, TasksModule,
   ],
 })
 export class AppModule {}
